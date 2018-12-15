4e47c77e17ec4d1db6ef363e115760a7170f98f4
TriQuach <qmtri@apcs.vn>
2018-12-15 01:53:27 -0500

    data freq

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ valid_id = ['A434F11F1B05', 'A434F11EEE06', 'A434F11F1684', 'A434F11F1E86', 'A43
 				'A434F1204005', 'A434F11F1F03', 'A434F11F3902', 'A434F11EF68F', 'A434F11F1106', 'A434F11F1782',
 				'A434F11F1607', 'A434F11F4287', 'A434F11F1F02', 'A434F11F1406', 'A434F11F0E85', 'A434F11EEF8C',
 				'A434F11F1E09', 'A434F11F0E03', 'A434F11F1483', 'A434F11F1F85']
+numProperty = 4 #Temperature;Humidity;AirPressure;Voltage
+
+
+k = np.zeros((5, 5))
 
 class Sensor:
     def __init__(self, time, id_sensor, temperature, humidity, airPressure, voltage):
@@ -24,9 +28,9 @@ class Sensor:
         self.airPressure = airPressure
         self.voltage = voltage
 
-sensor0 = Sensor(0,0,0,0,0,0)
+initSensor = Sensor(0,0,0,0,0,0)
 dicts = {
-    '1': [sensor0]
+    '1': [initSensor]
 }
 
 def hashTable():
@@ -52,28 +56,53 @@ def readfile():
         sensor = Sensor(word[0],word[1],word[2],word[3],word[4],word[5])
         dicts[sensor.id_sensor].append(sensor)
 
+def getFreq(start,end,tableFreq):
+    for i in valid_id:
+        listSensor = dicts[i]
+        print(i)
+        for idx, sensor in enumerate(listSensor):
+            if(idx < len(listSensor)-1):
+                if (int(sensor.time) >= int(start) and int(sensor.time) <= int(end)):
+                    if (sensor.temperature != listSensor[idx+1].temperature):
+                        indexxSensor = valid_id.index(i)
+                        tableFreq[indexxSensor][0] += 1
+                    if (sensor.humidity != listSensor[idx+1].humidity):
+                        indexxSensor = valid_id.index(i)
+                        tableFreq[indexxSensor][1] += 1
+                    if (sensor.airPressure != listSensor[idx+1].airPressure):
+                        indexxSensor = valid_id.index(i)
+                        tableFreq[indexxSensor][2] += 1
+                    if (sensor.voltage != listSensor[idx+1].voltage):
+                        indexxSensor = valid_id.index(i)
+                        tableFreq[indexxSensor][3] += 1
+                elif (int(sensor.time) > int(end)):
+                    break
+
+
 def testDict():
-    temp = dicts['A434F11EA281']
-    for i in temp:
-        print(i.humidity)
+    count = 0
+    sen = dicts['A434F11EEE06']
+    for idx,i in enumerate(sen):
+        if (idx < len(sen)-3):
+            if (i.voltage != sen[idx+1].voltage):
+                count += 1
+    print(count)
+
 hashTable()
 removeDefaultValue()
 readfile()
-testDict()
-
-k = np.zeros((5,5))
-k[0][1] = 2
-
-print(k)
+# print(tableFreq)
 app = Flask(__name__)
 # http://127.0.0.1:5000/frequency?start=1&end=2
 @app.route('/frequency')
 def frequency():
+    tableFreq = np.zeros((len(valid_id), numProperty))
     start = request.args.get('start', None)
     end = request.args.get('end', None)
+    getFreq(start,end,tableFreq)
     return jsonify (
-        k.tolist()
+        tableFreq.tolist()
     )
-    # return "asd"
+
 
 
